1bb9cb9c76f1eb5a7e55acd4f308ca067b348664
root <root@localhost.localdomain>
2021-01-17 16:29:02 +0800

    更新配置文件

---
+++ add-custom-script-1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: add-custom-script-1.sh
+# Description: Add script in update feeds before
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+#加入对指定软件包源码的远程仓库的克隆指令
+#git clone https://github.com/P3TERX/xxx package/xxx
+
